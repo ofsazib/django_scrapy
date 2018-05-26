c148961ed5d7653a3e2a8493d2262ffd6cde6ff9
Omar Faruk Sazib <ofsazib@gmail.com>
2018-05-27 02:24:52 +0600

    Added scrapyitem model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
+import json
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ScrapyItems(models.Model):
+    unique_id = models.CharField(max_length=100, blank=True)
+    data = models.TextField()
+    date = models.DateField(default=timezone.now)
+
+    @property
+    def to_dict(self):
+        data = {
+            'data': json.loads(self.data),
+            'date': self.date
+        }
+        return data
+
+    def __str__(self):
+        return self.unique_id
